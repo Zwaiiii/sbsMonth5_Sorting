1cc1b37d6d9e72e06bbb4963ac676fd7aa189af4
Zwaiiii <rlatlgns9099@gmail.com>
2026-01-12 21:23:22 +0900

    2026_01_12 astar

--- sbsMonth5_Sorting/AstarPathFinding.cpp
+++ sbsMonth5_Sorting/AstarPathFinding.cpp
@@ -1,7 +1,18 @@
 #pragma once
 #include<Windows.h>
+#include<vector>
+#include <stdexcept>
+#include<queue>
+#include<cmath>
+
+using namespace std;
+
 #define MapWidth 30
 #define MapHeight 30
+#define GoalX 25
+#define GoalY 5
+#define StartX 5
+#define StartY 25
 
 enum Color
 {
@@ -28,7 +39,7 @@ void HideCursor();
 void SetPosition(int x, int y);
 void ChangeColor(Color color);
 #pragma endregion
-
+#pragma region Board
 int map[MapHeight][MapWidth] =
 {
 	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
@@ -42,11 +53,11 @@ int map[MapHeight][MapWidth] =
 	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
 	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
 	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
 	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
 	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
 	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
@@ -62,12 +73,94 @@ int map[MapHeight][MapWidth] =
 	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
 	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
 };
+int manhattan[MapHeight][MapWidth];
+vector<vector<pair<int, bool>>> visited(MapHeight, vector<pair<int,bool>>(MapWidth, {INT_MAX, false}));
+priority_queue<Node> pq;
+struct Node {
+	int y;
+	int x;
+	int level;
+	int distance;
+
+	Node(int y, int x, int level)
+		: y(y), x(x), level(level),
+		distance(manhattan[y][x]) {}
+
+	bool operator<(const Node& other) const {
+		return level + distance > other.level + other.distance;
+	}
+};
+#pragma endregion
+//////////////////////////////////////////////////////////////////////////
+#pragma region Astar
+void Init() {
+	if (map[StartY][StartX] != 1) map[StartY][StartX] = 2;
+	else throw runtime_error("시작지점이 벽입니다.");
+	if (map[GoalY][GoalX] != 1) map[GoalY][GoalX] = 3;
+	else throw runtime_error("도착지접이 벽입니다.");
+
+	for (int i = 0; i < MapHeight; i++) {
+		for (int j = 0; j < MapWidth; i++) {
+			if (map[i][j] == 1) manhattan[i][j] = -1;
+			else manhattan[i][j] = abs(GoalY - i) + abs(GoalX - j);
+		}
+	}
+
+	pq.emplace(StartY, StartX, 0);
+}
+
+void PrintBoard() {
+	for (int y = 0; y < 30; y++)
+	{
+		for (int x = 0; x < 30; x++)
+		{
+			switch (map[y][x])
+			{
+			case 1:
+				SetPosition(x, y);
+				ChangeColor(White);
+				printf("■");
+				break;
+			case 4:
+				SetPosition(x, y);
+				ChangeColor(Red);
+				printf("■");
+				break;
+			case 5:
+				SetPosition(x, y);
+				ChangeColor(Green);
+				printf("■");
+				break;
+			default:
+				break;
+			}
+		}
+	}
+}
+#pragma endregion
+
 
 int main() {
 	HideCursor();
 
 	while (true) {
 		system("cls");
+
+		PrintBoard();
+
+		if (pq.empty()) throw runtime_error("큐가 비었습니다.");
+
+		auto node = pq.top(); pq.pop();
+
+		if (visited[node.y][node.x].second == true) {
+			if (visited[node.y][node.x].first < node.level)
+				continue;
+			else visited[node.y][node.x].first = node.level;
+		}
+		else {
+			visited[node.y][node.x] = make_pair(node.level, true);
+		}
+
 
 
 	}
